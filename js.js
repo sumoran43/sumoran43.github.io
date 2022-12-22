d8488eba9ee0c9b4c937198ac52b5fff9c0654c3
sumoran.github.io <susimor37@gmail.com>
2022-12-22 12:22:42 +0100

    archivos

---
+++ js.js
@@ -0,0 +1,34 @@
+
+function subrayado(photoshop) {
+    
+   photoshop.style.textDecoration = "underline";
+    
+}
+
+function sinsub(photoshop){
+    
+     photoshop.style.textDecoration = "none";
+}
+
+function subrayado(vectorial) {
+    
+   vectorial.style.textDecoration = "underline";
+    
+}
+
+function sinsub(vectorial){
+    
+     vectorial.style.textDecoration = "none";
+}
+
+function subrayado(tresd) {
+    
+   tresd.style.textDecoration = "underline";
+    
+}
+
+function sinsub(tresd){
+    
+     tresd.style.textDecoration = "none";
+}
+
